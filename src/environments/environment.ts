039fc8fe14542da48326e4d9997f6565ede2174b
Louise BERTIN <louise@trustfolio.co>
2023-05-09 08:13:47 +0200

    Init firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'angularts-7ef1f',
+    appId: '1:987390864029:web:6b266e709ac22504508274',
+    storageBucket: 'angularts-7ef1f.appspot.com',
+    apiKey: 'AIzaSyB8S0N7HP2zTidzJ0d-s74HWOvDsZ-Mwss',
+    authDomain: 'angularts-7ef1f.firebaseapp.com',
+    messagingSenderId: '987390864029',
+  }
+}
